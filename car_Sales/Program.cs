59429424080a871b5a00a84cba9f56a3a13dba3d
Adem Koçdoğan <admkcdn@gmail.com>
2023-12-28 22:08:12 +0300

    new branch

--- car_Sales/Program.cs
+++ car_Sales/Program.cs
@@ -45,7 +45,6 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
